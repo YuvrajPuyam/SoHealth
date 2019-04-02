ca0655b8f4ead9d45a546be583104f9bfdf561a9
Yuvraj Puyam <yuvrajpuyam@gmail.com>
2019-04-02 20:08:22 +0530

    Add files via upload

---
+++ SoHealth/SoHealth/README.md
@@ -0,0 +1,14 @@
+# SoHealth
+
+A Social Media WebApp where you can share your fitness data
+
+## How to run:
+
+Clone the git repo on to your local machine ```git clone https://github.com/ShauryaAg/SoHealth.git```
+
+#### Install the Dependencies: 
+
+Change the directory to .../SoHealth and,
+Install the dependencies using ```pip install -r requirements.txt```
+
+and run the command ```python run.py```
---
+++ SoHealth/SoHealth/forms.py
@@ -0,0 +1,73 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, RadioField,SelectField, FloatField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from SoHealth.models import User
+from wtforms_components import TimeField,  DateField
+
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    activity =  SelectField('Activity', choices =[('Walking', 'Walking'), ('Running', 'Running'),
+                                        ('Football', 'Football'), ('Basketball', 'Basketball'), ('Tennis', 'Tennis')])
+    time = TimeField('Total Time', format='%M:%S', validators=[DataRequired()])
+    kilometers = FloatField('Kilometers')
+    content = TextAreaField('Notes', validators=[DataRequired()])
+    submit = SubmitField('POST')
+
+class WeightForm(FlaskForm):
+    weight= FloatField('Enter Weight',validators=[DataRequired()])
+    Date = DateField("Enter Date",validators=[DataRequired()])
+    submit = SubmitField('POST')
